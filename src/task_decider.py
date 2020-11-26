c3dbf3062bee434cfcd8e64daecf9d4a4994d663
Laura <almasilaura@outlook.com>
2020-11-26 15:24:35 +0000

    defined all tests needed, and created the function for them to test, NOT RUNNING as expected yet

--- src/task_decider.py
+++ src/task_decider.py
@@ -5,9 +5,14 @@ class Task():
         self.duration = duration
 
     
-    def wash_dishes_preferred_over_cook_dinner(self, task_1, task_2):
-        #if task_1 == wash_dishes and task_2 == cook_dinner:
-            return wash_dishes.description
+    def decide_tasks(self, task_1, task_2):
+        
+        if task_1 == wash_dishes and task_2 == cook_dinner:
+            return task_1.description
+        elif task_1 == cooking_dinner and task_2 == cleaning_windows:
+            return task_1.description
+        elif task_1 == cleaning_windows and task_2 == cooking_dinner:
+            return task_1.description
 
     # def decide_task(self, task_1, task_2):
     #     if task_1 == wash_dishes and task_2 == cook_dinner:
--- tests/task_decider_test.py
+++ tests/task_decider_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.task_decider import *
+from src.task_decider import Task
 
 class TestTask_Decider(unittest.TestCase):
 
@@ -8,16 +8,26 @@ class TestTask_Decider(unittest.TestCase):
         self.wash_dishes = Task("wash dishes", 20)
         self.cooking_dinner = Task("cooking dinner", 30) 
         self.cleaning_windows = Task("cleaning windows", 40)
+
+
+    def wash_dishes_has_description(self):
+        self.assertEqual("wash dishes", self.wash_dishes.description)
+
+    def cooking_dinner_has_description(self):
+        self.assertEqual("cooking dinner", self.cooking_dinner.description)
    
+    def cleaning_windows_has_description(self):
+        self.assertEqual("cleaning windows", self.cleaning_windows.description)
+
+
     def test_wash_dishes_preferred_over_cook_dinner(self):
-       #wash_dishes_preferred_over_cook_dinner(self, was, task_2)
-        self.assertEqual("wash dishes", self.wash_dishes.description) #wash_dishes_preferred_over_cook_dinner(self, wash_dishes, cooking_dinner))
+        #decide_tasks(wash_dishes, cooking_dinner)
+        self.assertEqual("wash dishes", self.decide_tasks(wash_dishes, cooking_dinner)) 
 
-        # result = wash_dishes_preferred_over_cook_dinner(wash_dishes, cooking_dinner)
-        # self.assertEqual("wash dishes", result)
+    def test_cook_dinner_preferred_clean_windows(self):
+        self.assertEqual("cooking dinner", self.cooking_dinner.description)
 
-    # def test_cook_dinner_preferred_clean_windows(self):
-    #     self.assertEqual("cooking dinner", self.task.cooking_dinner)
+    def test_cleaning_windows_preferred_over_wash_dishes(self):
+        self.assertEqual("cleaning windows", self.cleaning_windows.description)
 
-    # def test_cleaning_windows_preferred_over_wash_dishes(self):
-    #     self.assertEqual("cleaning windows", self.task.cleaning_windows)
+        
